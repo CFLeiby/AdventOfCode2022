8c95e0318f1be2d707952c5c143df38fdeeb7757
Chick Leiby <chickleiby@gmail.com>
2022-12-16 08:39:32 -0500

    Day 2

--- 3.Rucksacks/Program.cs
+++ 3.Rucksacks/Program.cs
@@ -8,20 +8,48 @@ internal class Program
     {
         var priorities = 0;
         var sacks = File.ReadLines("Input.txt");
-        // if (args?.Length > 0)
-        //     priorities = sacks.Sum(r => { return GetScoreBasedOnOutcome(r); });
-        // else
+        if (args?.Length > 0)
+        {
+            var lastStart = 0;
+            var totalSacks = sacks.Count();
+            while(lastStart < totalSacks)
+            {
+                try
+                {
+                    priorities += GetBadgePriority(sacks.Skip(lastStart).Take(3));
+                }
+                catch (Exception e)
+                {
+                    Console.Write(e);
+                }
+                lastStart += 3;
+            }
+        }
+        else
             priorities = sacks.Sum(s => { return GetSackPriority(s); });
 
         Console.WriteLine(priorities);
     }
 
+    private static int GetBadgePriority(IEnumerable<string> sacks)
+    {
+        var sackItems = sacks.Select(s => s.Distinct()).ToArray();
+        foreach(var item in sackItems[0])
+        {
+            if (sackItems[1].Contains(item) && sackItems[2].Contains(item))
+            {
+                return ItemPriorty(item);
+            }
+        }
+        return 0;
+    }
+
     private static int GetSackPriority(string sack)
     {
-        var splitPoint = sack.Length/2;
-        var comp1 = sack.Substring(0, splitPoint);
-        var comp2 = sack.Substring(splitPoint);
-        foreach(var item in comp1.Distinct())
+        var halfSack = sack.Length/2;
+        var comp1 = sack.Substring(0, halfSack).Distinct();
+        var comp2 = sack.Substring(halfSack).Distinct();
+        foreach(var item in comp1)
         {
             if (comp2.Contains(item))
                 return ItemPriorty(item);
@@ -30,8 +58,8 @@ internal class Program
     }
 
     private static int ItemPriorty(char item)
-    {
-        var value = (int)item - 64;
-        return value < 27 ? value : value - 6;
+    {        
+        var value = (int)item;
+        return value < 91 ? value - 38: value - 96;
     }
 }
